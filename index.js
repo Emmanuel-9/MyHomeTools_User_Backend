64e4a107add46d1c2a2c884cc3e632cc4b88428a
Christine Bai <bai20726@gmail.com>
2022-03-18 12:07:30 +0300

    added the route for cart

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ require ('./config/db').connect();
 
 //call express app
 const app = express();
-app.use (express.json())
+app.use(express.json())
 
 app.use("/product", require('./routes/products'))
+app.use("/cart", require ('./routes/cartRoute'))
+
+
 
 //specify the port
 const port = process.env.PORT
