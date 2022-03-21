5844e679cd13934e1aff80c90c301c8b190576f5
Emmanuel-9 <oregadaniel181@gmail.com>
2022-03-21 08:48:31 +0300

    Remove the commented lines.

--- index.js
+++ index.js
@@ -19,17 +19,4 @@ app.listen (port, () => {
     console.log ('server listening on port ' + port)
 })
       
-//       app.use("/product", require('./routes/products'))
-//       app.use("/cart", require ('./routes/cartRoute'))
-      
-      
-//       app.use("/orders", require('./routes/orders'))
-      
-//       //specify the port
-//       const port = process.env.PORT
-      
-//       //Listen for connection
-//       app.listen (port, () => {
-//           console.log ('server listening on port ' + port)
-//       })
 module.exports = app
