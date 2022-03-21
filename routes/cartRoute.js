b086cf9da1ddbecad9caeecdcd6dc07961e0b85c
Emmanuel-9 <oregadaniel181@gmail.com>
2022-03-21 08:52:26 +0300

    Add the routes for get, add  and delte methods.

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const cartController = require('../controllers/cartController')
 
 //defining the routes
-router.get('/get', cartController.getCart);
-router.post('/add', cartController.addCart);
-router.put('/update', cartController.editCart);
-router.delete('/delete', cartController.deleteCart);
+router.get('/:user_id', cartController.getCart);
+router.post('/add/:user_id', cartController.addCart);
+router.delete('/delete/:user_id', cartController.removeCart);
+
 
 module.exports = router;
